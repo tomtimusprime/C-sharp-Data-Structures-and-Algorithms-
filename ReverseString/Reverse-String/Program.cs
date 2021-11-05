0a7d7f9210dae807ab74193dd9c4965839ae453f
Tom Black <tom.black06007@gmail.com>
2021-11-05 15:58:29 -0600

    Added a simple reverse string algorithm.

---
+++ ReverseString/Reverse-String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverseString("Thomas Black"));
+        }
+
+        public static string reverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
